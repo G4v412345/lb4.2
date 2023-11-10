61b418949960c9dc82cf32bd29a15949ffce8648
G4v412345 <131289952+G4v412345@users.noreply.github.com>
2023-11-10 11:32:45 +0200

    Add files via upload

---
+++ lb4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,133 @@
+﻿
+class TCircle
+{
+    protected double radius;
+
+    public TCircle()
+    {
+        radius = 0;
+    }
+
+    public TCircle(double r)
+    {
+        radius = r; 
+    }
+
+    public TCircle(TCircle circle)
+    {
+        radius = circle.radius;
+    }
+
+    public virtual void Input_Val()
+    {
+        Console.Write("Змiнити радiус: "); 
+        radius = Convert.ToDouble(Console.ReadLine());
+    }
+
+    public void Out_Val()
+    {
+        Console.WriteLine($"Радiус кола: {radius}");
+    }
+
+    public double Area()
+    {
+        return Math.PI * radius * radius;
+    }
+    public double Circumference() 
+    {
+        return 2 * Math.PI * radius;
+    }
+
+    public bool CompareWith(TCircle otherCircle)
+    {
+        return this.radius == otherCircle.radius;
+    }
+
+    public static TCircle operator +(TCircle circle1, TCircle circle2)
+    {
+        return new TCircle(circle1.radius + circle2.radius);
+    }
+
+    public static TCircle operator -(TCircle circle1, TCircle circle2)
+    {
+        return new TCircle(circle1.radius - circle2.radius);
+    }
+
+    public static TCircle operator *(TCircle circle, double factor)
+    {
+        return new TCircle(circle.radius * factor);
+    }
+}
+
+class TCylinder : TCircle
+{
+    private double height;
+
+    public TCylinder() : base()
+    {
+        height = 0;
+    }
+
+    public TCylinder(double r, double h) : base(r)
+    {
+        height = h;
+    }
+
+    public TCylinder(TCylinder cylinder) : base(cylinder)
+    {
+        height = cylinder.height;
+    }
+
+    public override void Input_Val()
+    {
+        base.Input_Val();
+        Console.Write("Змiнити висоту цилiндру: ");
+        height = Convert.ToDouble(Console.ReadLine());
+    }
+
+    public new void Out_Val()
+    {
+        base.Out_Val();
+        Console.WriteLine($"Висота цилiндра: {height}");
+    }
+
+    public double Volume() 
+    {
+        return base.Area() * height;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        TCircle circle1 = new TCircle(5);
+        TCircle circle2 = new TCircle(7);
+
+        TCircle circle3 = circle1 + circle2;
+        TCircle circle4 = circle1 - circle2;
+        TCircle circle5 = circle1 * 2;
+
+        Console.Write("Circle1: "); 
+        circle1.Out_Val();
+        Console.Write("Circle2: ");
+        circle2.Out_Val();
+        Console.Write("Circle3: ");
+        circle3.Out_Val();
+
+        if(circle1.CompareWith(circle3))
+        {
+            Console.WriteLine("cirlce1 == circle3"); 
+        }
+        else
+        {
+            Console.WriteLine("cirlce != circle3"); 
+        }
+
+        TCylinder cylinder = new TCylinder(4.0,5.0);
+        cylinder.Out_Val();
+        double x = cylinder.Volume();
+        Console.WriteLine($"V = {x}"); 
+        Console.ReadLine();
+    }
+}
---
+++ lb4/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,157 @@
+﻿class TFraction
+{
+    protected int numerator;
+    protected int denominator;
+
+    public TFraction()
+    {
+        numerator = 0;
+        denominator = 1;
+    }
+
+    public TFraction(int num, int den)
+    {
+        if (den == 0)
+        {
+            throw new ArgumentException("Знаменик не може бути нулем");
+        }
+
+        numerator = num;
+        denominator = den;
+    }
+
+    public TFraction(TFraction fraction)
+    {
+        numerator = fraction.numerator;
+        denominator = fraction.denominator;
+    }
+
+    public void Input_Val()
+    {
+        Console.Write("Напиши чисельник: ");
+        numerator = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Напиши знаменник: ");
+        int inputDenominator = Convert.ToInt32(Console.ReadLine());
+
+        if (inputDenominator == 0)
+        {
+            throw new ArgumentException("Знаменик не може бути нулем");
+        }
+
+        denominator = inputDenominator;
+    }
+
+    public void Out_Val()
+    {
+        Console.WriteLine($"{numerator}/{denominator}");
+    }
+
+    private int FindGCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public void Simplify()
+    {
+        int gcd = FindGCD(numerator, denominator);
+        numerator /= gcd;
+        denominator /= gcd;
+    }
+
+    public static TFraction operator +(TFraction fraction1, TFraction fraction2)
+    {
+        int resultNumerator = fraction1.numerator * fraction2.denominator + fraction2.numerator * fraction1.denominator;
+        int resultDenominator = fraction1.denominator * fraction2.denominator;
+        return new TFraction(resultNumerator, resultDenominator);
+    }
+
+    public static TFraction operator -(TFraction fraction1, TFraction fraction2)
+    {
+        int resultNumerator = fraction1.numerator * fraction2.denominator - fraction2.numerator * fraction1.denominator;
+        int resultDenominator = fraction1.denominator * fraction2.denominator;
+        return new TFraction(resultNumerator, resultDenominator);
+    }
+
+    public static TFraction operator *(TFraction fraction1, TFraction fraction2)
+    {
+        int resultNumerator = fraction1.numerator * fraction2.numerator;
+        int resultDenominator = fraction1.denominator * fraction2.denominator;
+        return new TFraction(resultNumerator, resultDenominator);
+    }
+
+    public static TFraction operator /(TFraction fraction1, TFraction fraction2)
+    {
+        if (fraction2.numerator == 0) 
+        {
+            throw new DivideByZeroException("Дiлити на нуль неможливо");
+        }
+
+        int resultNumerator = fraction1.numerator * fraction2.denominator;
+        int resultDenominator = fraction1.denominator * fraction2.numerator;
+        return new TFraction(resultNumerator, resultDenominator);
+    }
+}
+
+class TMixFraction : TFraction
+{
+    private int wholePart;
+
+    public TMixFraction() : base()
+    {
+        wholePart = 0;
+    }
+
+    public TMixFraction(int whole, int num, int den) : base(num, den)
+    {
+        wholePart = whole;
+    }
+
+    public TMixFraction(TMixFraction mixFraction) : base(mixFraction)
+    {
+        wholePart = mixFraction.wholePart;
+    }
+
+    public new void Out_Val()
+    {
+        Console.WriteLine($"{wholePart} {base.numerator}/{base.denominator}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        TFraction fraction1 = new TFraction(1, 2);
+        TFraction fraction2 = new TFraction(3, 4);
+
+        TFraction sum = fraction1 + fraction2;
+        TFraction difference = fraction1 - fraction2;
+        TFraction multi = fraction1 * fraction2;
+        TFraction quotient = fraction1 / fraction2;
+
+        Console.WriteLine("Сума:");
+        sum.Out_Val();
+
+        Console.WriteLine("Рiзниця:");
+        difference.Out_Val();
+
+        Console.WriteLine("Множення:");
+        multi.Out_Val(); 
+
+        Console.WriteLine("Частка:");
+        quotient.Out_Val();
+
+        TMixFraction mixFraction = new TMixFraction(2, 1, 3);
+        Console.WriteLine("Мiшана дрiб:");
+        mixFraction.Out_Val();
+
+        Console.ReadLine();
+    }
+}
